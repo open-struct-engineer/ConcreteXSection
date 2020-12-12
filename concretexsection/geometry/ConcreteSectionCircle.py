3acd4a2c8267a307a80496b940c3da92ece2af60
buddyd16 <donnieb@gmail.com>
2020-12-11 22:55:39 -0500

    additional section property calculations

--- concretexsection/geometry/ConcreteSectionCircle.py
+++ concretexsection/geometry/ConcreteSectionCircle.py
@@ -54,11 +54,72 @@ class ConcreteSectionCircle:
 
     def calc_props(self):
 
-        self.area = 2*math.pi*self.r*self.
+        self.area = 2*math.pi*self.r*self.r
 
         self.cx = 0
         self.cy = 0
         self.Ix =(1/4.0)*math.pi*math.pow(self.r,4)
         self.Iy = self.Ix
-
+        self.Ixy = 0
         self.Jz = self.Ix + self.Iy
+        self.sx_top = self.Ix / (self.r - self.cy)
+        self.sx_bottom = self.sx_top
+        self.sy_right = self.Iy / (self.r - self.cx)
+        self.sy_left = self.sy_right
+
+        self.rx = math.sqrt(self.Ix/self.area)
+        self.ry = math.sqrt(self.Iy/self.area)
+        self.rz = math.sqrt(self.Jz/self.area)
+
+        # properties about the cross section centroidal x and y axis
+        # Circle is centered on (0,0), therefore
+        # the properties above are the centroidal properties
+
+        self.Ixx = self.Ix
+        self.Iyy = self.Iy
+        self.Ixxyy = self.Ixy
+        self.Jzz = self.Ixx + self.Iyy
+        self.sxx_top = self.sx_top
+        self.sxx_bottom = self.sx_bottom
+        self.syy_right = self.sy_right
+        self.syy_left = self.sy_left
+
+        self.rxx = self.rx
+        self.ryy = self.ry
+        self.rzz = self.rz
+
+        # Cross section principle Axis
+
+        two_theta = math.atan((-1.0*2.0*self.Ixxyy)/(1E-16+(self.Ixx - self.Iyy)))
+        temp = (self.Ixx+self.Iyy)/2.0
+        temp2 = (self.Ixx-self.Iyy)/2.0
+        I1 = temp + math.sqrt((temp2*temp2)+(self.Ixxyy*self.Ixxyy))
+        I2 = temp - math.sqrt((temp2*temp2)+(self.Ixxyy*self.Ixxyy))
+
+        self.Iuu = temp + temp2*math.cos(two_theta) - self.Ixxyy*math.sin(two_theta)
+        self.Ivv = temp - temp2*math.cos(two_theta) + self.Ixxyy*math.sin(two_theta)
+        self.Iuuvv = temp2*math.sin(two_theta) + self.Ixxyy*math.cos(two_theta)
+
+        if I1-0.000001 <= self.Iuu <= I1+0.000001:
+            self.theta1 = math.degrees(two_theta/2.0)
+            self.theta2 = self.theta1 + 90.0
+        elif I2-0.000001 <= self.Iuu <= I2+0.000001:
+            self.theta2 = math.degrees(two_theta/2.0)
+            self.theta1 = self.theta2 - 90.0
+
+    def parallel_axis_theorem(self, x, y):
+        '''
+        given a new global x,y coordinate for a new
+        set of x, y axis return the associated Ix, Iy, and Ixy
+        '''
+        if self.area == 0:
+            return [0,0,0]
+        else:
+            dx = self.cx - x
+            dy = self.cy - y
+
+            Ix = self.Ixx + (self.area*dy*dy)
+            Iy = self.Iyy + (self.area*dx*dx)
+            Ixy = self.Ixxyy + (self.area*dx*dy)
+
+            return [Ix,Iy,Ixy]
